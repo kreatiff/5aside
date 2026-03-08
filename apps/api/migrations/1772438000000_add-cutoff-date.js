fa57957405342e8d04d1eb48e73926f4e2a121ce
kreatiff <oleg.estrin@gmail.com>
2026-03-08 10:37:45 +1000

    more fixes

--- apps/api/migrations/1772438000000_add-cutoff-date.js
+++ apps/api/migrations/1772438000000_add-cutoff-date.js
@@ -1,8 +1,10 @@
 /** @type {import("node-pg-migrate").MigrationBuilder} */
-exports.up = (pgm) => {
+export const shorthands = undefined;
+
+export const up = (pgm) => {
   pgm.sql(`ALTER TABLE settings ADD COLUMN cutoff_date DATE DEFAULT NULL`);
 };
 
-exports.down = (pgm) => {
+export const down = (pgm) => {
   pgm.sql(`ALTER TABLE settings DROP COLUMN cutoff_date`);
 };
